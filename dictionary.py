3cca77443f3657e4eadc69c9adf2f2304a29c51f
tdubliture <tdubliture@gmail.com>
2019-08-05 13:36:23 -0600

    dictionary stuff

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+"""
+Learn dictionaries
+dictionaries maps keys to values
+
+in some languages are known as associative arrays, or hashes or maps
+
+create them using {} containing a key-value pain
+retrieve them by key value
+retrieve them by [key_value]
+"""
+d = {'alice':'801-123-45-8989',
+     'pedro':'999-999-45-0000',
+     'john:': '123-456-78-9000'}
+print(d, type(d))
+#access one element
+print(d['pedro'])
