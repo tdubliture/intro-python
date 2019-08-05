1e301b6d3d95a404c1d363295a9cbb12ffeef231
tdubliture <tdubliture@gmail.com>
2019-08-05 12:52:03 -0600

    capitalization

--- strings.py
+++ strings.py
@@ -22,3 +22,12 @@ print(s4)
 #support for backslash
 s5 = "A\\in a string"
 print(s5)
+
+#Raw strings
+raw_string = r'C:\User\Documents\Books'
+print(raw_string)
+
+#string as a sequence
+s = "parrot"
+print("s[4]", s[4], type(s)) #index notation: 0,1,2,3,4
+print(s.capitalize())
