53ff751b4c9843055dd26c870830d12c404bd915
tdubliture <tdubliture@gmail.com>
2019-08-05 11:06:20 -0600

    added if elif

--- control_flow.py
+++ control_flow.py
@@ -7,5 +7,23 @@ learn about control flow in python
 if True:
     print("it is true")
 
-print("done")
+if bool("eggs"):
+    print("yes please!")
 
+if "eggs":
+    print("yes why not")
+
+#flat is better than nested
+h=42
+if h > 50:
+    print("greater than 50")
+    if h > 100:
+        print ("greater than 100")
+elif h < 50:
+    print ("less than 50")
+else:
+    print ("equal to 50")
+
+
+#######
+print("done")
