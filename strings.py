a510b51528e1da72f08f4c47ff7676512a98eb28
tdubliture <tdubliture@gmail.com>
2019-08-05 11:30:36 -0600

    added while loop

---
+++ strings.py
@@ -0,0 +1,20 @@
+"""
+strings and collections
+
+a string is an immutable sequence of unicode code-points
+"""
+print('this is a string')
+print ("this is a string too")
+
+#concatenation and adjacent strings
+s1 = "First"
+s2 = "Second"
+print(s1 + s2)
+
+#multiline strings and newlines
+s3 = """this is 
+a multi-line
+string"""
+print(s3)
+s4 = "this is\na multi-line\nstirng"
+print(s4)
