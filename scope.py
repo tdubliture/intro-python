157729844d3823342e369559dfb992249c10beef
tdubliture <tdubliture@gmail.com>
2019-08-06 14:13:58 -0600

    added documentation

---
+++ scope.py
@@ -0,0 +1,25 @@
+"""
+File info here
+"""
+count = 0  # global object
+
+
+def show_count():
+    """
+    display the current count
+    :return: nothing
+    """
+    print(count)
+
+
+def main():
+    """
+    Test function
+    :return: 
+    """
+    show_count()
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
