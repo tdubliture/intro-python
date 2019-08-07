6813a5d6333fc924fa3f9c502e2f4111cc5d3674
tdubliture <tdubliture@gmail.com>
2019-08-07 08:13:02 -0600

    catch up

--- scope.py
+++ scope.py
@@ -1,25 +1,73 @@
 """
-File info here
+
+Learn about scoping in Python
+
 """
-count = 0  # global object
+
+count = 0    # global object
+
+
+
 
 
 def show_count():
+
     """
-    display the current count
+
+    Display the current count
+
     :return: nothing
+
     """
+
     print(count)
 
 
+
+
+
+def set_count(num):
+
+    """
+
+    Set global counter to input
+
+    :param num: input number
+
+    :return: nothing
+
+    """
+
+    global count
+
+    count = num
+
+
+
+
+
 def main():
+
     """
+
     Test function
-    :return: 
+
+    :return:
+
     """
+
+    show_count()
+
+    set_count(9)
+
     show_count()
 
 
+
+
+
 if __name__ == '__main__':
+
     main()
-    exit(0)
+
+    exit(0)(0)
