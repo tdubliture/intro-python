0b07e1d008341a1b28b54cc719c9aaa8fe6825a5
tdubliture <tdubliture@gmail.com>
2019-08-08 16:00:22 -0600

    generator

--- use_airtravel.py
+++ use_airtravel.py
@@ -26,10 +26,10 @@ def make_flight():
 
 def console_card_printer(passenger,seat,flight_number,aircraft):
     output = "| Name: {0}"\
-            "Flight: {1}"\
-            "Seat: {2}"\
-            "Aircraft: {3}"\
-            "|".format(passenger,seat,flight_number,aircraft)
+            " Flight: {1} "\
+            " Seat: {2} "\
+            " Aircraft: {3} "\
+            "|".format(passenger,flight_number,seat,aircraft)
 
     banner = "+" + "-" * (len(output)-2)+ "+"
     border = "|" + " " * (len(output)-2)+ "|"
