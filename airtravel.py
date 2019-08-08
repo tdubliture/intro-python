ff06a1db3549c1b71b3c3c410528b72d3df1f9b6
tdubliture <tdubliture@gmail.com>
2019-08-08 15:56:27 -0600

    generator

--- airtravel.py
+++ airtravel.py
@@ -75,6 +75,34 @@ class Flight:
         self._seating[row][letter] = passenger
 
 
+    def num_available_seats(self):
+        """
+        determines the number of available seats
+        :return:
+        """
+        return sum(sum(1 for s in row.values() if s is None)
+                   for row in self._seating if row is not None)
+
+
+    def make_boarding_class(self, card_printer):
+        """
+        makes boarding pass
+        :param card_printer:
+        :return:
+        """
+        for passenger, seat in sorted(self._passenger_seats()):
+            card_printer(passenger,seat,self.number(), self._aircraft.model())
+
+
+    def _passenger_seats(self):
+        row_numbers, seat_letters = self._aircraft.seating_plan()
+        for row in row_numbers:
+            for letter in seat_letters:
+                passenger = self._seating[row][letter]
+                if passenger is not None:
+                    yield (passenger, "{}{}".format(row, letter))
+
+
 
 class Aircraft:
     """
--- use_airtravel.py
+++ use_airtravel.py
@@ -4,22 +4,52 @@ use flight class
 from airtravel import Flight, Aircraft
 from pprint import pprint as pp
 
+
+def make_flight():
+    """
+
+    :return:
+    """
+
+    flight = Flight("SN066", Aircraft("G-EUP", "Airbus A319",
+                                  num_rows=22,
+                                  num_seats_per_row=6))
+
+    flight.allocate_seat("02A", "Guido Van Rossum")
+    flight.allocate_seat("12B", "Rasmus Lerdorf")
+    flight.allocate_seat("15F", "Bjanre Stroustrup")
+    flight.allocate_seat("03A", "Larry Wall")
+    flight.allocate_seat("16F", "Yukihiro Mastsumoto")
+
+    return flight
+
+
+def console_card_printer(passenger,seat,flight_number,aircraft):
+    output = "| Name: {0}"\
+            "Flight: {1}"\
+            "Seat: {2}"\
+            "Aircraft: {3}"\
+            "|".format(passenger,seat,flight_number,aircraft)
+
+    banner = "+" + "-" * (len(output)-2)+ "+"
+    border = "|" + " " * (len(output)-2)+ "|"
+    lines = [banner,border,output,border,banner]
+    card = '\n'.join(lines)
+    print(card)
+
+
+
 def main():
     """
     Test function
     :return: 
     """
-    f1 = Flight("SN066",Aircraft("G-EUP","Airbus A319",
-                                 num_rows=22,
-                                 num_seats_per_row=6))
+    flight_1 = make_flight()
+    pp(flight_1._seating)
+    print("available seats", flight_1.num_available_seats())
 
-    f1.allocate_seat("02A", "Guido Van Rossum")
-    f1.allocate_seat("12B", "Rasmus Lerdorf")
-    f1.allocate_seat("15F", "Bjanre Stroustrup")
-    f1.allocate_seat("03A", "Larry Wall")
-    f1.allocate_seat("16F", "Yukihiro Mastsumoto")
+    flight_1.make_boarding_class(console_card_printer)
 
-    pp(f1._seating)
 
 if __name__ == '__main__':
     main()
